52e9f95cf6776e7a267bb14215678c96caec1329
Andrew <thisisandrewhi@gmail.com>
2023-04-11 20:44:21 +0800

    Client for tank --version 0.3

--- src/TankClient.java
+++ src/TankClient.java
@@ -2,7 +2,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class TankClient extends Frame {
-
+	public void paint(Graphics g) { 
+		Color c = g.getColor(); 
+		g.setColor(Color.RED); 
+		g.fillOval(50, 50, 30, 30); 
+		g.setColor(c);
+	}
+	
 	public void launchFrame() {
 		this.setLocation(300, 100);
 		this.setSize(800, 600);
@@ -12,8 +18,8 @@ public class TankClient extends Frame {
 				System.exit(0);
 			}
 		});
-		setResizable(false);
-		setVisible(true);
+		this.setResizable(false);
+		this.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
