7711d0a382466ed08415384cedb604117c26f296
Andrew <thisisandrewhi@gmail.com>
2023-04-11 20:38:17 +0800

    Client for tank --version 0.2

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,13 +1,24 @@
 import java.awt.*;
+import java.awt.event.*;
 
 public class TankClient extends Frame {
 
 	public void launchFrame() {
 		this.setLocation(300, 100);
-		this.setSize(800, 600); setVisible(true);
+		this.setSize(800, 600);
+		this.setTitle("TankWar"); 
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		setResizable(false);
+		setVisible(true);
 	}
 	
-	public static void main(String[] args) { TankClient tc = new TankClient(); tc.launchFrame();
+	public static void main(String[] args) {
+		TankClient tc = new TankClient();
+		tc.launchFrame();
 	
 	}
 }
